32ec965b95735c606796729a36c585904756ff5f
Vipul Guru <125081495+VG3627@users.noreply.github.com>
2024-08-11 11:13:33 +0530

    edited sender.py

--- sender.py
+++ sender.py
@@ -48,6 +48,7 @@ def send_multicast(group_ip, port, interface_ip, file_paths):
             
             with open(file_path, 'rb') as file:
                 index = 1  # Use a consistent index for all chunks of this file
+                byte_sent = 0 
                 while chunk_cnt > 0:
                     chunk = file.read(BUFFER_SIZE)
                     if not chunk:
@@ -55,14 +56,16 @@ def send_multicast(group_ip, port, interface_ip, file_paths):
                     # Send chunk with file-specific index
                     # chunk_data = f"{file_name}|{index}|".encode() + chunk
                     sock.sendto(chunk, (group_ip, port))
+                    byte_sent += len(chunk)
                     
                     
                    
                     chunk_cnt -= 1 
                     time.sleep(0.001)  # Small delay to avoid overwhelming the network
                     
-            
+            print('\n')
             print(f"File {file_name} sent to {group_ip}:{port} from interface {interface_ip}")
+            print(f"{byte_sent} bytes sent to of {file_size}")
 
     except socket.timeout:
         print("Socket timeout occurred. Consider increasing the timeout duration.")
